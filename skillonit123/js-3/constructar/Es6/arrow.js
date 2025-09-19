2608dde85688b24dcbfa74170d45096f3e16ee68
Your Name <sureshmanke3@gmail.com>
2025-09-19 17:59:27 +0530

    last update

---
+++ skillonit123/js-3/constructar/Es6/arrow.js
@@ -0,0 +1,14 @@
+// // arrow function 
+// // shortcut syntax for function 
+
+
+// shorter function
+function add(a,b){
+          console.log(a+b)
+}
+let x=add(10,20);
+console.log(x)
+
+function add(a,b){
+          return a+b;
+}
